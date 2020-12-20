0b091fa610ceb29370936d3c7c997ba9a01ec7e2
Emprinnos <emprinnos@pop-os.localdomain>
2020-12-19 22:29:03 -0600

    proyecto hecho

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.ham span');
+
+
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+    ham.classList.toggle('girar');
+});
+
